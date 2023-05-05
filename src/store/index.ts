1bd678c20ef62ab24a9f9478c1d9848721c18a7b
cloudedpanther <rickeykoh@ajou.ac.kr>
2023-05-06 01:31:27 +0900

    Refactor: Add isLoggedIn file

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,28 +1,3 @@
-import { atom, selector } from 'recoil'
+import { isLoggedInState, isLoggedInSelector } from './isLoggedIn'
 
-export const isLoggedInState = atom({
-    key: 'isLoggedIn',
-    default: selector({
-        key: 'localIsLoggedIn',
-        get: () => {
-            const json = localStorage.getItem(isLoggedInState.key)
-
-            if (!json) return false
-
-            const isLoggedIn = JSON.parse(json)
-
-            return isLoggedIn
-        },
-    }),
-})
-
-export const isLoggedInSelector = selector({
-    key: 'isLoggedInSelector',
-    get: ({ get }) => get(isLoggedInState),
-    set: ({ set }, newValue) => {
-        set(isLoggedInState, newValue)
-
-        const json = JSON.stringify(newValue)
-        localStorage.setItem(isLoggedInState.key, json)
-    },
-})
+export { isLoggedInState, isLoggedInSelector }
